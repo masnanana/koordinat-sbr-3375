b4edfa73b8b38b3f11089fd63a783b7e7a595484
masnanana <skripsimasna@gmail.com>
2025-06-24 15:22:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Cek Lokasi Gmaps", layout="centered")
 
 st.markdown("""
     <div style="text-align: center;">
-        <img src="https://raw.githubusercontent.com/masnanana/koordinat-sbr-3375/main/logo_tirto.png" width="300"/>
+        <img src="https://raw.githubusercontent.com/masnanana/koordinat-sbr-3375/main/logo_tirtoo.png" width="300"/>
         <div style="font-size:16px; margin-top:5px; color: #444;"> <h1 style="margin-top:10px;">Temukan Informasi Rukun Tetangga Otomatis</h1></div>
        
     </div>
