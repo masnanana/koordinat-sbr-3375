f3427becce26bdfdb36796d7089e717cb4ff2e40
masnanana <skripsimasna@gmail.com>
2025-06-24 15:11:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Cek Lokasi Gmaps", layout="centered")
 # === Judul dengan Logo dan Tagline
 st.markdown("""
     <div style="text-align: center;">
-        <img src="https://raw.githubusercontent.com/masnanana/assets-public/main/logo_tirto.png" width="100"/>
+        <img src="https://raw.githubusercontent.com/masnanana/koordinat-sbr-3375/main/logo_tirto.png" width="100"/>
         <div style="font-size:16px; margin-top:5px; color: #444;">Temukan Informasi Rukun Tetangga Otomatis</div>
         <h1 style="margin-top:10px;">Cek Wilayah SLS Kota Pekalongan</h1>
     </div>
